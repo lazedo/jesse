2d7c5dba6e22895bec5e5b7a26512f8a2836d310
lazedo <luis.azedo@factorlusitano.com>
2019-02-01 02:16:42 +0000

    cleanup errors if oneOf passes validation

--- src/jesse_validator_draft4.erl
+++ src/jesse_validator_draft4.erl
@@ -1196,11 +1196,11 @@ check_one_of(_Value, _InvalidSchemas, State) ->
   handle_schema_invalid(?wrong_one_of_schema_array, State).
 
 check_one_of_(_Value, [], State, 1) ->
-  State;
+  jesse_state:set_error_list(State, []);
 check_one_of_(Value, [], State, 0) ->
-  handle_data_invalid(?not_one_schema_valid, Value, State);
+  handle_data_invalid(?not_one_schema_valid, Value, jesse_state:set_error_list(State, []));
 check_one_of_(Value, _Schemas, State, Valid) when Valid > 1 ->
-  handle_data_invalid(?not_one_schema_valid, Value, State);
+  handle_data_invalid(?not_one_schema_valid, Value, jesse_state:set_error_list(State, []));
 check_one_of_(Value, [Schema | Schemas], State, Valid) ->
   case validate_schema(Value, Schema, State) of
     {true, NewState} ->
